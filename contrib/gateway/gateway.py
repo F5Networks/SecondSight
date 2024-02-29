a4b572bee68088a20360a0e92fed0895411084c6
Fabrizio Fiorucci <fiorucci@oasi.asti.it>
2024-02-29 13:49:49 +0000

    20240229-01 Commit - Second Sight Gateway

--- contrib/gateway/gateway.py
+++ contrib/gateway/gateway.py
@@ -1,12 +1,11 @@
-import pathlib
 import uvicorn
-import subprocess
 import json
 import uuid
+import requests
 
 from typing import Any, Dict, AnyStr, List, Union
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, File, UploadFile
 from fastapi.responses import PlainTextResponse, Response, JSONResponse
 
 app = FastAPI(
@@ -19,22 +18,41 @@ JSONObject = Dict[AnyStr, Any]
 JSONArray = List[Any]
 JSONStructure = Union[JSONArray, JSONObject]
 
-@app.post("/v1/collector", status_code=200, response_class=JSONResponse)
-def post_collector(response: Response, request: JSONStructure = None):
-    if request:
-        try:
-            sessionUUID = uuid.uuid4()
-            jsonPayload = json.dumps(request)
 
-            print(f"JSON Payload: {jsonPayload}")
+#
+# Payload is the BIG-IQ tarball
+# curl 127.0.0.1:5000/api/v1/archive -X POST -F "file=@/path/filename.ext"
+#
+@app.post("/api/v1/archive", status_code=200, response_class=JSONResponse)
+async def v1_post_archive(file: UploadFile = File(...)):
+    # Reset the file pointer to the beginning
+    file.file.seek(0)
 
-            # Payload to upstream endpoint
+    # Endpoint
+    endpoint = 'https://192.168.2.19:443/upload'
 
-            return JSONResponse (content={'status': 'success'}, status_code=200)
-        except Exception as e:
-            return JSONResponse (content={'status': str(e)}, status_code=400)
-    else:
-        return JSONResponse (content={'status': 'invalid body'}, status_code=400)
+    # mTLS authentication
+    #mTLS_cert = ('/path/client.crt', '/path/client.key')
+
+    multipart_form_data = {
+        'tarball': ('tarball.zip', file.file)
+    }
+
+    # Publish to F5 telemetry endpoint
+    r = requests.post(url = endpoint,
+                      files = multipart_form_data,
+                      auth = ('admin','default'),
+                      verify = False,
+                      #cert = mTLS_cert
+                      )
+
+    print(f"Response HTTP status: {r.status_code}")
+    print(f"Response Body       : {r.content.decode('utf-8')}")
+
+    output = json.loads(r.content.decode('utf-8'))
+    output['filename'] = file.filename
+
+    return output
 
 if __name__ == '__main__':
     uvicorn.run("gateway:app", host='0.0.0.0', port=5000)
